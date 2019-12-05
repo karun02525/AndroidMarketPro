94555b0def1a62ffc9d13dfd11a5bd896d0815c8
kaju02525 <karunkumar02525@gmail.com>
2019-12-05 08:30:44 +0530

    fixed language change

--- app/src/main/java/com/pusauli/user/language_change/SettingsActivity.kt
+++ app/src/main/java/com/pusauli/user/language_change/SettingsActivity.kt
@@ -3,11 +3,9 @@ package com.pusauli.user.language_change
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.pusauli.user.R
 import com.pusauli.user.ui.dashboard.MainActivity
-import com.pusauli.user.utils.toast
 import kotlinx.android.synthetic.main.activity_language.*
 
 class SettingsActivity : AppCompatActivity() {
@@ -34,14 +32,12 @@ class SettingsActivity : AppCompatActivity() {
 
         ll_english.setOnClickListener {
             langCode="en"
-            toast("English")
         }
-
 
         ll_hindi.setOnClickListener {
             langCode="hi"
-            toast("Hindi")
         }
+
 
 
         btn_continue.setOnClickListener {
@@ -50,6 +46,7 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
     }
+
 
 
 }
